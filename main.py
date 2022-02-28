43b0c7fc78d627c84d2fc0a38377ac92ab084bc5
nd <andriy.haluza@gmail.com>
2022-02-28 15:00:04 +0200

    feat: update

--- main.py
+++ main.py
@@ -1,44 +1,105 @@
 import asyncio
 import random
+import pymongo
+import os
+import datetime
 
+from dotenv import load_dotenv
 from telethon import TelegramClient
 from telethon import functions, types
-import questionary
 
-api_id = int(questionary.password('Api ID:').ask())
-api_hash = questionary.password('Api hash:').ask()
+load_dotenv()
 
+myClient = pymongo.MongoClient("mongodb://localhost:27017/")
 
+db = myClient["telegram_report_bot_ua"]
 
-client = TelegramClient('session_new', api_id, api_hash)
+channelsCollection = db['channels']
+
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
+
+client = TelegramClient('session_new', API_ID, API_HASH)
 client.start()
 
 print('Bot started')
 
 
-async def main():
-    number_of_channels_rep = 150
+async def fillDB():
+    print('Fill Db started')
 
     telegram_list = open('telegram_db', 'r').readlines()
-    random.shuffle(telegram_list)
 
-    for (i,telegram_channel) in enumerate(telegram_list[:number_of_channels_rep]):
+    for (i, telegram_channel) in enumerate(telegram_list):
         if "https://" in telegram_channel:
             telegram_channel = telegram_channel.split('/')[-1]
         elif '@' in telegram_channel:
             telegram_channel = telegram_channel[1:]
-        print(i+1, telegram_channel.strip())
         try:
+            telegram_channel = telegram_channel.split('\n')[0]
+            splittedWithPriority = telegram_channel.split('|')
+
+            if len(splittedWithPriority) == 1:
+                name = splittedWithPriority[0]
+            elif len(splittedWithPriority) == 2:
+                name = splittedWithPriority[0]
+                priority = int(splittedWithPriority[1])
+
+            if not priority:
+                priority = 0
+
+            isInDb = channelsCollection.find_one({"name": name})
+
+            if not isInDb:
+                channelsCollection.insert_one({
+                    "name": name,
+                    "priority": priority,
+                    "usageCount": 0,
+                    "lastUsage": None,
+                })
+
+        except ValueError:
+            print("Channel not found")
+
+
+async def main():
+    await fillDB()
+    await runBot()
+
+
+async def runBot():
+    channels = channelsCollection.aggregate([{"$match": {}}, {"$sort": {
+        "lastUsage": 1,
+        "priority": -1,
+    }}])
+
+    for (i, telegram_channel) in enumerate(channels):
+        try:
+            print(telegram_channel)
+            message = 'Propaganda of the war in Ukraine. Propaganda of the murder of Ukrainians and Ukrainian soldiers.' + \
+                str(random.random())
+
             result = await client(functions.account.ReportPeerRequest(
-                peer=telegram_channel,
+                peer=telegram_channel["name"],
                 reason=types.InputReportReasonSpam(),
-                message='RUSSIAN PROPAGANDA AGAINST UKRAINE DURING RUSSIAN INVASION IN UKRAINE' + str(random.random())
-            ))
+                message=message)
+            )
             print(result)
+
+            updateObject = {
+                "$set": {
+                    "lastUsage": datetime.datetime.now(),
+                },
+                "$inc": {
+                    "usageCount": 1
+                }
+            }
+
+            channelsCollection.update_one(
+                {"_id": telegram_channel["_id"]}, updateObject)
+
         except ValueError:
             print("Channel not found")
         await asyncio.sleep(10 + 2 * random.random())
 with client:
-
     client.loop.run_until_complete(main())
-
